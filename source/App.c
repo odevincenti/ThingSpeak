3b5ddc98794cb779017315b0f35094b44244d03d
odevincenti <odevincenti@itba.edu.ar>
2022-11-11 16:08:09 -0300

    Updates del labo

--- source/App.c
+++ source/App.c
@@ -27,8 +27,16 @@
 #define KEEPALIVE_MS	1000
 #define DATA_MS			15*1000
 
+#define PISO1_IDX		5
+#define PISO2_IDX		7
+#define PISO3_IDX		9
+
 enum {IS_OK, IS_FAIL, IS_ERR};
 enum {IDLE, KEEPALIVE_SEND, KEEPALIVE_CHECK, DATA_SEND, DATA_CHECK};
+
+#define GET_MSBYTE(x)	((uint8_t) ((x) >> 8) )
+#define GET_LSBYTE(x)	((uint8_t) ((x) & LSB_MASK) )
+#define LSB_MASK		0x0F
 
 /*******************************************************************************
  * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
@@ -43,10 +51,12 @@ uint8_t IsKeepAliveOk();
 /*******************************************************************************
  * STATIC VARIABLES AND CONST VARIABLES WITH FILE LEVEL SCOPE
 *******************************************************************************/
-static const uint8_t header[] = {0xAA, 0x55, 0xC3, 0x3C};
-static uint16_t piso1 = 0;
-static uint16_t piso2 = 0;
-static uint16_t piso3 = 0;
+// static uint8_t keeepalive_msg[] = {0xAA, 0x55, 0xC3, 0x3C, 0x01, 0x02};
+static uint8_t keepalive_msg[] = {'A', '5', '3', '5', '1', '2'};
+static uint8_t data_msg[] = {'A', '5', '3', '5', '7', '0', '0', '0', '0', '0', '0'};
+static uint16_t piso1 = 0x3131;
+static uint16_t piso2 = 0x3232;
+static uint16_t piso3 = 0x3333;
 
 static tim_id_t keepalive_timer;
 static tim_id_t data_timer;
@@ -86,8 +96,10 @@ void App_Run (void)
 			break;
 		case KEEPALIVE_CHECK:
 			if (uartIsTxMsgComplete(UART_ID)){
-				if (IsKeepAliveOk() != KEEPALIVE_OK){
-					// prender led rojo o algo idk
+				if (IsKeepAliveOk() == KEEPALIVE_OK){
+					// Prender led keepalive
+				} else {
+					// Apagar led
 				}
 				state = state == KEEPALIVE_CHECK ? IDLE : state;
 			}
@@ -98,8 +110,8 @@ void App_Run (void)
 			break;
 		case DATA_CHECK:
 			if (uartIsTxMsgComplete(UART_ID)){
-				if (IsDataOk() != DATA_OK){
-					// prender led rojo o algo idk
+				if (IsDataOk() == DATA_OK){
+					// Volver a mandar data
 				}
 				state = state == DATA_CHECK ? IDLE : state;
 			}
@@ -118,15 +130,13 @@ void App_Run (void)
 
 void SendData(){
 
-	uint8_t length = 0x07;
-	uint8_t command = 0x01;
-	
-	uartWriteMsg(UART_ID, (uint8_t*) &header[0], sizeof(header));
-	uartWriteMsg(UART_ID, &length, 1);
-	uartWriteMsg(UART_ID, &command, 1);
-	uartWriteMsg(UART_ID, (uint8_t*) &piso1, 2);
-	uartWriteMsg(UART_ID, (uint8_t*) &piso2, 2);
-	uartWriteMsg(UART_ID, (uint8_t*) &piso3, 2);
+	data_msg[PISO1_IDX] = GET_MSBYTE(piso1);
+	data_msg[PISO1_IDX + 1] = GET_LSBYTE(piso1);
+	data_msg[PISO2_IDX] = GET_MSBYTE(piso2);
+	data_msg[PISO2_IDX + 1] = GET_LSBYTE(piso2);
+	data_msg[PISO3_IDX] = GET_MSBYTE(piso3);
+	data_msg[PISO3_IDX + 1] = GET_LSBYTE(piso3);
+	uartWriteMsg(UART_ID, (uint8_t*) &data_msg[0], sizeof(data_msg));
 
 }
 
@@ -137,7 +147,7 @@ uint8_t IsDataOk(){
 	uint8_t length;
 	uint8_t answer;
 
-	uartReadMsg(UART_ID, (uint8_t*) &header_rx[0], sizeof(header));
+	uartReadMsg(UART_ID, (uint8_t*) &header_rx[0], 4);
 	uartReadMsg(UART_ID, &length, 1);
 	uartReadMsg(UART_ID, &answer, 1);
 
@@ -161,14 +171,7 @@ void Data_IRQ(){
 }
 
 void KeepAlive(){
-
-	uint8_t length = 0x01;
-	uint8_t command = 0x02;
-	
-	uartWriteMsg(UART_ID, (uint8_t*) &header[0], sizeof(header));
-	uartWriteMsg(UART_ID, &length, 1);
-	uartWriteMsg(UART_ID, &command, 1);
-
+	uartWriteMsg(UART_ID, (uint8_t*) &keepalive_msg[0], sizeof(keepalive_msg));
 }
 
 uint8_t IsKeepAliveOk(){
@@ -178,7 +181,7 @@ uint8_t IsKeepAliveOk(){
 	uint8_t length;
 	uint8_t answer;
 
-	uartReadMsg(UART_ID, (uint8_t*) &header_rx[0], sizeof(header));
+	uartReadMsg(UART_ID, (uint8_t*) &header_rx[0], 4);
 	uartReadMsg(UART_ID, &length, 1);
 	uartReadMsg(UART_ID, &answer, 1);
 
